4bf36a748da861c305a2290f6791568a30899b9f
Lakshya Bhardwaj <lakshyasharma6258@gmail.com>
2026-09-08 05:21:17 +0530

    Time: 0 ms (100%), Space: 42.1 MB (75.71%) - LeetHub

---
+++ 0461-hamming-distance/0461-hamming-distance.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int hammingDistance(int x, int y) {
+        int ans = x^y;
+        int cnt = 0;
+        for(int i = 0;i<32;i++){
+            if((ans & (1<<i)) != 0 ){
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+}
